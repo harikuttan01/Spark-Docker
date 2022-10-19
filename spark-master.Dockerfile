279e690157127482fda98cd114b85bd869f84e9c
Hareendran VR <hareendran.vr@lntinfotech.com>
2022-10-19 16:10:28 +0530

    ..

--- spark-master.Dockerfile
+++ spark-master.Dockerfile
@@ -4,5 +4,6 @@ FROM hareendranvr/spark-base
 
 ARG spark_master_web_ui=8080
 
+
 EXPOSE ${spark_master_web_ui} ${SPARK_MASTER_PORT}
 CMD bin/spark-class org.apache.spark.deploy.master.Master >> logs/spark-master.out
--- test.py
+++ test.py
@@ -3,8 +3,9 @@ from pyspark.sql import SparkSession
 myspark = SparkSession.\
         builder.\
         appName("pyspark").\
-        master("spark://192.168.1.106:7077").\
+        master("spark://127.0.0.1:7077").\
         config("spark.executor.memory", "512m").\
         getOrCreate()
 
-myspark.read.csv('iris.data')
+# myspark.read.csv('iris.data')
+print(myspark)
