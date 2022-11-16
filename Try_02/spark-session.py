c9ec3e227730b69c17df1588016cbe607ae0b764
Hareendran VR <hareendran.vr@lntinfotech.com>
2022-11-16 16:59:26 +0530

    ..

--- Try_02/spark-session.py
+++ Try_02/spark-session.py
@@ -27,7 +27,7 @@ def get_memory(memory):
 def get_configs():
 
     return {
-        # "spark.kubernetes.namespace": SparkDistributed.namespace,
+        "spark.kubernetes.namespace": SparkDistributed.namespace,
         "spark.kubernetes.container.image": SparkDistributed.executor_pod_image,
         "spark.kubernetes.container.image.pullSecrets": "regcred",
         "spark.kubernetes.container.image.pullPolicy": "Always",
@@ -46,42 +46,6 @@ def get_configs():
         # "spark.jars": "/opt/conda/lib/python3.7/site-packages/pyspark/jars"
         # https://spark.apache.org/docs/2.4.3/running-on-kubernetes.html -> Configuration
     }
-
-
-# def add_volume_mounts(config):
-#     vm = Volumes.volume_custom_mount(SparkDistributed.project_id, SparkDistributed.minio_data_bucket)[0]
-#     # spark.kubernetes.driver.volumes.[VolumeType].[VolumeName].mount.path -> for adding multiple volumes
-#     conf = "spark.kubernetes.executor.volumes.persistentVolumeClaim."
-#     config[conf + str(vm.get('name')) + ".options.claimName"] = SparkDistributed.pvc_name
-#     config[conf + str(vm.get('name')) + ".mount.path"] = vm.get("mountPath")
-#     config[conf + str(vm.get('name')) + ".mount.subPath"] = vm.get("subPath")
-#     config[conf + str(vm.get('name')) + ".mount.readOnly"] = vm.get("readOnly", "false")
-#     # For adding any custom volumes to executor.
-#     for i in Volumes.volume_mount_count(SparkDistributed.project_id, SparkDistributed.user_id):
-#         config[conf + str(i.get('name')) + ".options.claimName"] = str(i.get('name'))
-#         config[conf + str(i.get('name')) + ".mount.path"] = str(i.get('mountPath'))
-#         config[conf + str(i.get('name')) + ".mount.readOnly"] = "false"
-#     return config
-
-
-# def add_executor_env_variables(config):
-#     try:
-#         config["spark.executorEnv.PYTHONPATH"] = SparkDistributed.PYTHONPATH
-#         f = list(open("/notebooks/.bashrc", "r"))
-#         for i in f:
-#             x = " ".join(i.split(" ")[1:]).rstrip().split("=")
-#             # Removing single quotes used in file
-#             # export userId = 'data_user'
-#             # export custom_spark_jars_path = '/data/*'
-#             x[1] = x[1].strip("\'")
-#             config[f"spark.executorEnv.{x[0]}"] = x[1]
-#             if x[0] == "custom_spark_jars_path":
-#                 config[f"spark.driver.extraClassPath"] = x[1] if x[1] else "/data/*"
-#                 config[f"spark.executor.extraClassPath"] = x[1] if x[1] else "/data/*"
-#         return config
-#     except Exception as ex:
-#         print(f"EXCEPTION occurred while reading custom env variables: {ex}")
-#         return config
 
 
 def get_spark_session(app_name: str, conf: SparkConf):
